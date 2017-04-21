3019abad36cb30b1ebc44863a044b77c3e21b698
leobert-lan <774057695@qq.com>
2017-04-21 15:49:02 +0800

    Init

---
+++ actionsheet/src/main/java/individual/leobert/uilib/actionsheet/ViewHolder.java
@@ -0,0 +1,14 @@
+package individual.leobert.uilib.actionsheet;
+
+import android.widget.TextView;
+
+/**
+ * <p><b>Package</b> com.lht.customwidgetlib.actionsheet
+ * <p><b>Project</b> VsoCyy
+ * <p><b>Classname</b> ViewHolder
+ * <p><b>Description</b>: action sheet item view 布局复用
+ * Created by leobert on 2016/7/5.
+ */
+public class ViewHolder {
+    public TextView text;
+}
