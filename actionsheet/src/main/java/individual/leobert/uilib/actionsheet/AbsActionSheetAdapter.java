b6345c84f17bc7e93eb55d05c69174c3aaf4acf8
leobert-lan <774057695@qq.com>
2017-04-21 17:08:37 +0800

    [master] [upload actionsheet v1.0.0 to bintray]

--- actionsheet/src/main/java/individual/leobert/uilib/actionsheet/AbsActionSheetAdapter.java
+++ actionsheet/src/main/java/individual/leobert/uilib/actionsheet/AbsActionSheetAdapter.java
@@ -6,12 +6,12 @@ import android.widget.BaseAdapter;
 
 
 /**
- * @package com.lht.vsocyy.adapter
- * @project AndroidBase
- * @classname AbsListAdapter
- * @description: listview适配器抽象父类，抽象了getView，并用接口实现，减少getView中可能的依赖
- * 要区分一下adapter到底是V层的还是M层的真的很难。
- * Created by leobert on 2016/4/1.
+ * <p><b>Package</b> individual.leobert.uilib.actionsheet
+ * <p><b>Project</b> actionsheet
+ * <p><b>Classname</b> AbsActionSheetAdapter
+ * <p><b>Description</b>: 抽象listadapter
+ * <p>
+ * Created by leobert on 2017/4/21.
  */
 public abstract class AbsActionSheetAdapter extends BaseAdapter {
 
@@ -30,19 +30,6 @@ public abstract class AbsActionSheetAdapter extends BaseAdapter {
     @Override
     public abstract long getItemId(int position);
 
-    /**
-     * desc: TODO: 描述方法
-     * <p/>
-     * 这里我考虑过两种解耦方式：
-     * 1高度复用adapter，使用接口完成getView回调。
-     * 2定义click等相关接口
-     * 当前，我选用第一种形式，不会让activity显得过于臃肿
-     *
-     * @param position
-     * @param convertView
-     * @param parent
-     * @return
-     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return itemViewProvider.getView(position, getItem(position), convertView, parent);
