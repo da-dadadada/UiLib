2b7fabdcb00e476632c01c6742ec3560c28abde8
leobert-lan <774057695@qq.com>
2017-04-24 14:29:04 +0800

    [master] [add test and guide for numbadge_v1.0.0]

--- app/src/main/java/individual/leobert/uilib/TestNumBadge.java
+++ app/src/main/java/individual/leobert/uilib/TestNumBadge.java
@@ -2,12 +2,98 @@ package individual.leobert.uilib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TestNumBadge extends AppCompatActivity {
+import individual.leobert.uilib.numbadge.NumBadge;
+
+public class TestNumBadge extends AppCompatActivity implements View.OnClickListener{
+    private NumBadge badgeOval, badgeRectAngle, badgeDefault;
+    private NumBadge[] numBadges;
+
+    private Button btnTestDot;
+    private Button btnTestNum1;
+    private Button btnTestNum8;
+    private Button btnTestNum88;
+    private Button btnTestNum888;
+
+    /**
+     * the max num to display,if lager than it,will display as [MAX_BADGE_NUM]+
+     * e.g. use MAX_BADGE_NUM = 99,when you set 123 via #updateWithFriendlyMode(num,MAX_BADGE_NUM);
+     * will display as 99+
+     */
+    private static final int MAX_BADGE_NUM = 99;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_num_badge);
+
+        badgeOval = (NumBadge) findViewById(R.id.numbadge_1);
+        badgeRectAngle = (NumBadge) findViewById(R.id.numbadge_2);
+        badgeDefault = (NumBadge) findViewById(R.id.numbadge_3);
+
+        btnTestDot = (Button) findViewById(R.id.numbadge_test_1);
+        btnTestNum1 = (Button) findViewById(R.id.numbadge_test_2);
+        btnTestNum8 = (Button) findViewById(R.id.numbadge_test_3);
+        btnTestNum88 = (Button) findViewById(R.id.numbadge_test_4);
+        btnTestNum888 = (Button) findViewById(R.id.numbadge_test_5);
+
+        btnTestDot.setOnClickListener(this);
+        btnTestNum1.setOnClickListener(this);
+        btnTestNum8.setOnClickListener(this);
+        btnTestNum88.setOnClickListener(this);
+        btnTestNum888.setOnClickListener(this);
+
+
+        numBadges = new NumBadge[]{
+                badgeOval, badgeRectAngle, badgeDefault
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.numbadge_test_1:
+                testDot();
+                break;
+            case R.id.numbadge_test_2:
+                testSetNumActually(1);
+                break;
+            case R.id.numbadge_test_3:
+                testSetNumActually(8);
+//                testSetNumFriendly(8);
+                break;
+            case R.id.numbadge_test_4:
+                testSetNumActually(88);
+//                testSetNumFriendly(88);
+                break;
+            case R.id.numbadge_test_5:
+//                testSetNumActually(888);
+                testSetNumFriendly(888);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void testDot() {
+        for (NumBadge numBadge:numBadges) {
+            numBadge.updateWithPointMode();
+        }
+    }
+
+    private void testSetNumFriendly(int num) {
+        for (NumBadge numBadge:numBadges) {
+            numBadge.updateWithFriendlyMode(num,MAX_BADGE_NUM);
+        }
+    }
+
+    private void testSetNumActually(int num) {
+        for (NumBadge numBadge:numBadges) {
+            numBadge.updateWithActualMode(num);
+        }
     }
 }
---
+++ numbadge/ReadMe.md
@@ -0,0 +1,58 @@
+# Sences for using
+this is a libaray to display a num-badge in app,you cannot use it to display a badge at conner of the shortcut/launcher.
+# How to use NumBadge
+	step1 include lib into project
+enable repo:
+	
+	repositories {
+	    jcenter {
+	        url "http://jcenter.bintray.com/"
+	    }
+	}
+	
+use dependency in gradle:
+`compile 'individual.leobert.libs:numbadge:1.0.0'`
+
+sync gradle dependencies.
+
+---
+
+	step2 define in layout:
+e.g.
+
+	 <individual.leobert.uilib.numbadge.NumBadge
+	            android:id="@+id/numbadge_1"
+	            android:layout_width="wrap_content"
+	            android:layout_height="wrap_content"
+	            numbudge:backgroundShape="oval" />
+if you want to use attrs in xml,remenber to define xlmns:
+
+	xmlns:numbudge="http://schemas.android.com/apk/res-auto"
+	
+in version 1.0.0,only "backgroundShape" attribute provided. It's an enum includes 'oval'(default) and 'rectAngle'
+
+---
+
+	step3 update with api:
+
+* **public void updateWithPointMode();**
+
+	only display a point;
+
+* **public void updateWithFriendlyMode(int num, int max);**
+
+	**param max**:the max num to display,if num is lager than it,will display as [MAX_BADGE_NUM]+;
+
+	e.g. use MAX_BADGE_NUM = 99,when you set 123 via #updateWithFriendlyMode(num,MAX_BADGE_NUM);  will display as 99+
+   
+* **public void updateWithActualMode(int num);**
+
+	display the number you give,specially use for number "1",and different form #updateWithFriendlyMode(num,MAX_BADGE_NUM) when ***num*** is lager than ***max***
+
+* **public void clear();**
+
+	clear the num and hide display
+
+* **public void setBackgroundShape(Shape bgShape);**
+
+	change "backgroundShape"
